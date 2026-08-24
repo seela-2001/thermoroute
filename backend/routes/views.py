7d8db9538ef8702f10710fcf64061bb637c7a6a1
Sohila Mustafa <sohila@otomatika.tech>
2026-08-24 22:23:59 +0300

    allowed hosts + corsheaders

--- backend/routes/views.py
+++ backend/routes/views.py
@@ -1,12 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from .serializers import RouteAnalysisRequestSerializer
 from .services.route_analysis_service import RouteAnalysisService
 from .services.location_services import LocationService
 
 
 class RouteAnalysisView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def post(self, request):
         serializer = RouteAnalysisRequestSerializer(
@@ -53,6 +56,8 @@ class RouteAnalysisView(APIView):
         )
 
 class LocationAutocompleteView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def get(self, request):
 
